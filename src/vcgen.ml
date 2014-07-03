01de92e6f8e549f356e48182cd23003e7e6f029a
7jpinheiro <7jpinheiro@gmail.com>
2014-07-03 14:39:19 +0100

    Add computes cfg

--- src/vcgen.ml
+++ src/vcgen.ml
@@ -1,5 +1,30 @@
 open Cil_types
 
+(* Computes cfg for all functions and fills in info information on fundec (smaxstmid and sallsmts) *)
+let computeCfg () =
+	Globals.Functions.iter_on_fundecs
+	(
+      fun fundec -> 
+      	Cfg.prepareCFG fundec;
+      	Cfg.computeCFGInfo fundec false  
+	)
+
+
+(* Visits Statements *)
+let visitStatements fd fs =
+	Format.printf"Statements.\n"
+
+
+(* Visits functions *)
+let visitFunctions () = 
+	Globals.Functions.iter
+	(
+      fun kf -> 
+      	let fundec = Kernel_function.get_definition kf in
+      	let funspec = Annotations.funspec kf in 
+        visitStatements fundec funspec
+	)
+
   (* Main function *)
   let run () =
 
@@ -7,6 +32,7 @@ open Cil_types
      if (Ast.is_computed()) then Format.printf"AST computed.\n";
 
      let c_file = Ast.get() in
-     Cfg.computeFileCFG c_file
+     Cfg.clearFileCFG c_file;
+     computeCfg ()
      
 let () = Db.Main.extend run 
