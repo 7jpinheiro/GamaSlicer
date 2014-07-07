2021bac6398e2338c3fd2dea9bfd9006d9678967
7jpinheiro <7jpinheiro@gmail.com>
2014-07-07 17:51:17 +0100

    Add print_ss_postcondtion
    Now it computes a list of reversed statements and the function post condition and prints it to output

--- src/vcgen.ml
+++ src/vcgen.ml
@@ -1,5 +1,7 @@
 open Cil_types
 
+
+(* Gets option *)
 let get_opt = function
   | Some x -> x
   | None   -> raise (Invalid_argument "Empty Function behavior")
@@ -12,34 +14,71 @@ let computeCfg () =
       	Cfg.prepareCFG fundec;
       	Cfg.computeCFGInfo fundec false  
 	)
+	
+(* Prints a predicate(condition in this case) *)
+let print_condtion cond =
+	Format.printf"Post_condition: %a\n" Printer.pp_predicate_named cond
+
+(* Prints a list of statements *)
+let print_statements list_statements = 
+	List.iter
+		(
+		 fun s -> Format.printf"%a\n" Printer.pp_stmt s
+		) list_statements
+
+(* Prints a List of tuples of a list of statements and a condition *)
+let print_ss_postcondtion l =
+	List.iter
+	(
+	 fun (x,y) -> print_statements x;
+	 			  print_condtion y  
+	) l
+	
+
+(* Returns a reversed list of statements found in fundec.sallstmts after the computation of the cfg *)
+let get_list_of_statements fundec = 
+	Format.printf"Getting list of statements\n";
+	let list_statements = fundec.sallstmts in
+	List.rev list_statements
 
 
-(* Visits Statements *)
-let visitStatements fundec funspec list_behaviors =
+(* Get condition depeding ond the func_bulidcondtion input *)
+let get_Condtion  funspec func_buildcondition =
+	Format.printf"Getting Contdition\n";
 	let funbehavior = Cil.find_default_behavior funspec in
-	let post_condition = Ast_info.behavior_postcondition (get_opt funbehavior) Normal in
+	let post_condition =  func_buildcondition (get_opt funbehavior) Normal in
 	post_condition 
 
-(* Visits functions *)
-let visitFunctions () = 
-	Globals.Functions.iter
-	(
-      fun kf -> 
-      	let fundec = Kernel_function.get_definition kf in
+(* Visits and applys if there kf has definition *)
+let apply_if_defition def kf =
+	match def with
+	|true ->
+		let fundec = Kernel_function.get_definition kf in
       	let funspec = Annotations.funspec kf in 
       	let list_behaviors = Annotations.behaviors kf in 
-        let post_condt = visitStatements fundec funspec list_behaviors in
-        ()
-	)
+        let post_condt = get_Condtion funspec  Ast_info.behavior_postcondition  in
+        let list_statements = get_list_of_statements fundec in
+        [(list_statements,post_condt)]
+	|false -> []
+
+(* Visits functions *)
+let visitFunctions () =
+	Format.printf"Visting functions.\n";
+	Globals.Functions.fold
+	(
+      fun kf acc -> (apply_if_defition (Kernel_function.is_definition kf) kf) @ acc
+	) []
 
   (* Main function *)
   let run () =
 
      Ast.compute (); 
-     if (Ast.is_computed()) then Format.printf"AST computed.\n";
+     if (Ast.is_computed()) then Format.printf"AST computed.\n"; 	
 
-     let c_file = Ast.get() in
+     let c_file = Ast.get () in
      Cfg.clearFileCFG c_file;
-     computeCfg ()
+     computeCfg ();
+     let list_stm_and_post = visitFunctions () in
+     print_ss_postcondtion list_stm_and_post
      
 let () = Db.Main.extend run 
