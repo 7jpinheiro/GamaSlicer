f915c360bb3a16d96b8e3991e5fef00257b94a01
7jpinheiro <7jpinheiro@gmail.com>
2014-09-10 16:33:51 +0000

    Updated convert_pred2why
    Now outputs a why3 formula

--- src/vcgen.ml
+++ src/vcgen.ml
@@ -69,7 +69,7 @@ let () = Self.result "Loading Why3 theories..."
 let int_type : Ty.ty = Ty.ty_int
 let int_theory : Theory.theory =
   try
-    Env.find_theory env ["int"] "Int"
+    (Env.read_theory env ["int"] "Int")
   with e ->
     Self.fatal "Exception raised while loading int theory:@ %a"
       Exn_printer.exn_printer e
@@ -84,12 +84,12 @@ let gt_int : Term.lsymbol = find int_theory "infix >"
 let lt_int : Term.lsymbol = find int_theory "infix <"
 
 let computer_div_theory : Theory.theory =
-  Env.find_theory env ["int"] "ComputerDivision"
+  Env.read_theory env ["int"] "ComputerDivision"
 let div_int : Term.lsymbol = find computer_div_theory "div"
 
 (* real.Real theory *)
 let real_type : Ty.ty = Ty.ty_real
-let real_theory : Theory.theory = Env.find_theory env ["real"] "Real"
+let real_theory : Theory.theory = Env.read_theory env ["real"] "Real"
 let add_real : Term.lsymbol = find real_theory "infix +"
 let sub_real : Term.lsymbol = find real_theory "infix -"
 let minus_real : Term.lsymbol = find real_theory "prefix -"
@@ -97,7 +97,7 @@ let mul_real : Term.lsymbol = find real_theory "infix *"
 let ge_real : Term.lsymbol = find real_theory "infix >="
 
 (* map.Map theory *)
-let map_theory : Theory.theory = Env.find_theory env ["map"] "Map"
+let map_theory : Theory.theory = Env.read_theory env ["map"] "Map"
 let map_ts : Ty.tysymbol = find_type map_theory "map"
 (* let map_type (t:Ty.ty) : Ty.ty = Ty.ty_app map_ts [t] *)
 let map_get : Term.lsymbol = find map_theory "get"
@@ -105,105 +105,6 @@ let map_get : Term.lsymbol = find map_theory "get"
 
 let () = Self.result "Loading Why3 modules..."
 
-let find_ps mo s =
-  try
-    Mlw_module.ns_find_ps mo.Mlw_module.mod_export [s]
-  with e ->
-    Self.fatal "Exception raised while loading Why3 program symbol %s:@ %a"
-      s Exn_printer.exn_printer e
-
-let find_ls mo s = find mo.Mlw_module.mod_theory s
-
-(* ref.Ref module *)
-
-let ref_modules, ref_theories =
-  try
-    Env.read_lib_file (Mlw_main.library_of_env env) ["ref"]
-  with e ->
-    Self.fatal "Exception raised while loading ref module:@ %a"
-      Exn_printer.exn_printer e
-
-let ref_module : Mlw_module.modul = Stdlib.Mstr.find "Ref" ref_modules
-
-let ref_type : Mlw_ty.T.itysymbol =
-  Mlw_module.ns_find_its ref_module.Mlw_module.mod_export ["ref"]
-
-let ref_fun : Mlw_expr.psymbol = find_ps ref_module "ref"
-
-let get_logic_fun : Term.lsymbol = find_ls ref_module "prefix !"
-
-let get_fun : Mlw_expr.psymbol = find_ps ref_module "prefix !"
-
-let set_fun : Mlw_expr.psymbol = find_ps ref_module "infix :="
-
-(* mach_int.Int32 module *)
-
-let mach_int_modules, _mach_int_theories =
-  try
-    Env.read_lib_file (Mlw_main.library_of_env env) ["mach";"int"]
-  with e ->
-    Self.fatal "Exception raised while loading mach.int modules:@ %a"
-      Exn_printer.exn_printer e
-
-let int32_module : Mlw_module.modul =
-  try
-    Self.result "Looking up module mach.int.Int32";
-    Stdlib.Mstr.find "Int32" mach_int_modules
-  with Not_found ->
-    Self.fatal "Module mach.int.Int32 not found"
-
-let int32_type : Why3.Ty.tysymbol =
-  Mlw_module.ns_find_ts int32_module.Mlw_module.mod_export ["int32"]
-
-let int32_to_int : Term.lsymbol = find_ls int32_module "to_int"
-
-let add32_fun : Mlw_expr.psymbol = find_ps int32_module "infix +"
-
-let sub32_fun : Mlw_expr.psymbol = find_ps int32_module "infix -"
-
-let mul32_fun : Mlw_expr.psymbol = find_ps int32_module "infix *"
-
-let neg32_fun : Mlw_expr.psymbol = find_ps int32_module "prefix -"
-
-let eq32_fun : Mlw_expr.psymbol = find_ps int32_module "eq"
-
-let ne32_fun : Mlw_expr.psymbol = find_ps int32_module "ne"
-
-let le32_fun : Mlw_expr.psymbol = find_ps int32_module "infix <="
-
-let lt32_fun : Mlw_expr.psymbol = find_ps int32_module "infix <"
-
-let ge32_fun : Mlw_expr.psymbol = find_ps int32_module "infix >="
-
-let gt32_fun : Mlw_expr.psymbol = find_ps int32_module "infix >"
-
-let int32ofint_fun : Mlw_expr.psymbol = find_ps int32_module "of_int"
-
-(* mach_int.Int64 module *)
-
-let int64_module : Mlw_module.modul =
-  try
-    Self.result "Looking up module mach.int.Int64";
-    Stdlib.Mstr.find "Int64" mach_int_modules
-  with Not_found ->
-    Self.fatal "Module mach.int.Int64 not found"
-
-let int64_type : Why3.Ty.tysymbol =
-  Mlw_module.ns_find_ts int64_module.Mlw_module.mod_export ["int64"]
-
-let int64_to_int : Term.lsymbol = find_ls int64_module "to_int"
-
-let add64_fun : Mlw_expr.psymbol = find_ps int64_module "infix +"
-
-let sub64_fun : Mlw_expr.psymbol = find_ps int64_module "infix -"
-
-let mul64_fun : Mlw_expr.psymbol = find_ps int64_module "infix *"
-
-let le64_fun : Mlw_expr.psymbol = find_ps int64_module "infix <="
-
-let lt64_fun : Mlw_expr.psymbol = find_ps int64_module "infix <"
-
-let int64ofint_fun : Mlw_expr.psymbol = find_ps int64_module "of_int"
 
 let t_app ls args =
   try
@@ -225,25 +126,20 @@ let is_real_type t =
     | _ -> false
 
 
-(* Converts the generated predicates to stmt language *)
-let gen_po predicate = {proof_obligation = Term.t_true;}
-
-
 let get_logic_constant_value lc = 
 	match lc with
-	| Integer (integer,some_value) -> Number.ConstInt (Number.int_const_dec (Pervasives.string_of_int integer))
-	| LReal lr	    			   -> Number.ConstReal (Number.real_const_dec lr.r_literal)
+	| Integer (integer,some_value) -> Number.ConstInt (Number.int_const_dec (Pervasives.string_of_int (Integer.to_int integer)))
+	| LReal { r_literal = s }	   -> Number.ConstReal (Number.real_const_dec "" "" (Some s))
 	| LStr _ 					   -> raise (Invalid_argument "Logic constant with type: LStr not yet implemented")
 	| LWStr _ 					   -> raise (Invalid_argument "Logic constant with type: LWStr not yet implemented")
-	| LChar _ 					   -> raise (Invalid_argument "Logic constant with type: LChar not yet implemented")
+	| LChr _ 					   -> raise (Invalid_argument "Logic constant with type: LChar not yet implemented")
     | LEnum _ 					   -> raise (Invalid_argument "Logic constant with type: LEnum not yet implemented")
 
 let get_logic_var_value (t_host,t_offset) =
 	match t_host with
 	| TVar lc 	-> Term.create_vsymbol (Ident.id_fresh lc.lv_name ) Ty.ty_int  
-	| TResult 	-> raise (Invalid_argument "Logic var host with type: LResult not yet implemented")
-	| TMem  	-> raise (Invalid_argument "Logic var host with type: TMem not yet implemented")
-
+	| TResult _	-> raise (Invalid_argument "Logic var host with type: LResult not yet implemented")
+	| TMem  _	-> raise (Invalid_argument "Logic var host with type: TMem not yet implemented")
 
 let convert_unary2why unop term =
 	match unop with
@@ -251,7 +147,7 @@ let convert_unary2why unop term =
 	| BNot 	-> raise (Invalid_argument "Unary operation with type: BNot not yet implemented")
 	| LNot	-> raise (Invalid_argument "Unary operation with type: LNot not yet implemented")
 
-let rec convert_binary2why binop t1 t2 =
+let convert_binary2why binop t1 t2 =
 	match binop with 	
 	| PlusA			-> t_app add_int [t1;t2]
 	| MinusA  		-> t_app sub_int [t1;t2]
@@ -277,30 +173,31 @@ let rec convert_binary2why binop t1 t2 =
 	| MinusPP		-> raise (Invalid_argument "Binary operation with type: MinusPP not yet implemented")
 
 let rec convert_term2why term = 
-	| match term.term_node with
-	| TConst lc 				-> Term.t_const get_logic_constant_value lc
-	| TLval tvar 				-> Term.t_var get_logic_var_value tvar
+	match term.term_node with
+	| TConst lc 				-> Term.t_const (get_logic_constant_value lc)
+	| TLval tvar 				-> Term.t_var (get_logic_var_value tvar)
 	| TUnOp (unop,tnp1) 		-> convert_unary2why unop (convert_term2why tnp1)
 	| TBinOp (binop,tbp1,tbp2)	-> convert_binary2why binop (convert_term2why tbp1) (convert_term2why tbp2)
+	| TLogic_coerce (ty,terl)	-> convert_term2why terl
+	| Tat (tat1,ll1) 			-> convert_term2why tat1
+	| TCastE (ty1,cter1)		-> convert_term2why cter1
 	| TSizeOf _ 				-> raise (Invalid_argument "Logic term with type: TSizeOf not yet implemented")
 	| TSizeOfE _ 				-> raise (Invalid_argument "Logic term with type: TSizeOfE not yet implemented")
 	| TSizeOfStr _ 				-> raise (Invalid_argument "Logic term with type: TSizeOfStr not yet implemented")
 	| TAlignOf _ 				-> raise (Invalid_argument "Logic term with type: TAlignOf not yet implemented")
 	| TAlignOfE _ 				-> raise (Invalid_argument "Logic term with type: TAlignOfE not yet implemented")
-	| TCastE _ 					-> raise (Invalid_argument "Logic term with type: TCastE not yet implemented")
 	| TAddrOf _ 				-> raise (Invalid_argument "Logic term with type: TAddrOf not yet implemented")
 	| TStartOf _ 				-> raise (Invalid_argument "Logic term with type: TStartOf not yet implemented")
 	| Tapp _ 					-> raise (Invalid_argument "Logic term with type: Tapp not yet implemented")
 	| Tlambda _ 				-> raise (Invalid_argument "Logic term with type: Tlambda not yet implemented")
 	| TDataCons _ 				-> raise (Invalid_argument "Logic term with type: TDataCons not yet implemented")
 	| Tif _ 					-> raise (Invalid_argument "Logic term with type: Tif not yet implemented")
-	| Tat _ 					-> raise (Invalid_argument "Logic term with type: Tat not yet implemented")
 	| Tbase_addr _ 				-> raise (Invalid_argument "Logic term with type: Tbase_addr not yet implemented")
 	| Toffset _ 				-> raise (Invalid_argument "Logic term with type: Toffset not yet implemented")
 	| Tblock_length _ 			-> raise (Invalid_argument "Logic term with type: Tblock_length not yet implemented")
 	| Tnull						-> raise (Invalid_argument "Logic term with type: Tnull not yet implemented")
-	| TLogic_coerce _ 			-> raise (Invalid_argument "Logic term with type: TLogic_coerce not yet implemented")
-	| TCoerceE _ 				-> raise (Invalid_argument "Logic term with type: TCoerceE not yet implemented")
+	| TCoerce (_,_) 			-> raise (Invalid_argument "Logic term with type: TCoerce not yet implemented")
+	| TCoerceE (_,_) 			-> raise (Invalid_argument "Logic term with type: TCoerce not yet implemented")
 	| TUpdate _ 				-> raise (Invalid_argument "Logic term with type: TUpdate not yet implemented")
 	| Ttypeof _  				-> raise (Invalid_argument "Logic term with type: Ttypeof not yet implemented")
 	| Ttype _ 					-> raise (Invalid_argument "Logic term with type: Ttype not yet implemented")
@@ -319,20 +216,37 @@ let rec convert_rel2why rlt term1 term2 =
 	| Rgt 	-> t_app gt_int [term1;term2]
 	| Rle 	-> t_app le_int [term1;term2]
 	| Rge 	-> t_app ge_int [term1;term2]
-	| Req 	-> Term.t_equ (convert_term2why term1) (convert_term2why term2)
-	| Rneq 	-> Term.t_neq (convert_term2why term1) (convert_term2why term2)   
+	| Req 	-> Term.t_equ  term1 term2
+	| Rneq 	-> Term.t_neq  term1 term2 
 
 let rec convert_pred2why predicate = 
 	match predicate.content with
-	| Pfalse						-> Term.t_false
-	| Ptrue 						-> Term.t_true
-	| Pnot 		p_not 				-> Term.t_not (convert_pred2why p_not)
-	| Pand 		(pand1,pand2) 		-> Term.t_and (convert_pred2why pand1) (convert_pred2why pand2)
-	| Por 		(por1,por2) 		-> Term.t_or  (convert_pred2why por1) (convert_pred2why por2)
-	| Pimplies	(pim1,pim2) 		-> Term.t_implies (convert_pred2why pim1) (convert_pred2why pim2)
-	| Piff 		(piff1,piff2) 		-> Term.t_iff (convert_pred2why piff1) (convert_pred2why piff2)
-	| Pif 		(tif1,pif1,pif2) 	-> Term.t_if (convert_termw2hy tif1) (convert_pred2why pif1) (convert_pred2why pif2)
-	| Prel 		(rlt,trl1,trl2)		-> convert_rel2why rlt trl1 trl2
+	| Pfalse					-> Term.t_false
+	| Ptrue 					-> Term.t_true
+	| Pnot p_not 				-> Term.t_not (convert_pred2why p_not)
+	| Pand (pand1,pand2) 		-> Term.t_and (convert_pred2why pand1) (convert_pred2why pand2)
+	| Por (por1,por2) 			-> Term.t_or  (convert_pred2why por1) (convert_pred2why por2)
+	| Pimplies (pim1,pim2) 		-> Term.t_implies (convert_pred2why pim1) (convert_pred2why pim2)
+	| Piff (piff1,piff2) 		-> Term.t_iff (convert_pred2why piff1) (convert_pred2why piff2)
+	| Pif (tif1,pif1,pif2) 		-> Term.t_if (convert_term2why tif1) (convert_pred2why pif1) (convert_pred2why pif2)
+	| Prel (rlt,trl1,trl2)		-> convert_rel2why rlt (convert_term2why trl1) (convert_term2why trl2)
+	| Papp _ 					-> raise (Invalid_argument "Logic predicate with type: Papp not yet implemented")	
+	| Pseparated _  			-> raise (Invalid_argument "Logic predicate with type: Pseparated not yet implemented")
+	| Pxor _ 					-> raise (Invalid_argument "Logic predicate with type: Pxor not yet implemented")
+	| Plet _ 					-> raise (Invalid_argument "Logic predicate with type: Plet not yet implemented")
+	| Pforall _ 				-> raise (Invalid_argument "Logic predicate with type: Pforall not yet implemented")
+	| Pexists _ 				-> raise (Invalid_argument "Logic predicate with type: Pexists not yet implemented")
+	| Pat  _ 					-> raise (Invalid_argument "Logic predicate with type: Pat not yet implemented")
+	| Pvalid_read _ 			-> raise (Invalid_argument "Logic predicate with type: Pvalid_read not yet implemented")
+	| Pvalid _ 					-> raise (Invalid_argument "Logic predicate with type: Pvalid not yet implemented")
+	| Pinitialized _ 			-> raise (Invalid_argument "Logic predicate with type: Pinitialized not yet implemented")
+	| Pallocable _ 				-> raise (Invalid_argument "Logic predicate with type: Pallocable not yet implemented")
+	| Pfreeable _ 				-> raise (Invalid_argument "Logic predicate with type: Pfreeable not yet implemented")
+	| Pfresh _ 					-> raise (Invalid_argument "Logic predicate with type: Pfresh not yet implemented")
+	| Psubtype _ 				-> raise (Invalid_argument "Logic predicate with type: Psubtype not yet implemented")
+
+(* Converts the generated predicates to stmt language *)
+let gen_po predicate = {proof_obligation = convert_pred2why predicate;}
 
 (* Builds vcgen_result with simple type *)
 let build_vcgen_result_simple statement predicate  =
@@ -368,22 +282,26 @@ let get_opt = function
 
 
 (* Prints a predicate(condition in this case) *)
+let print_why3_term term =
+	Self.result "Why3 Formula: %a\n" Pretty.print_term term
+
+(* Prints a predicate(condition in this case) *)
 let print_condtion cond =
-	Format.printf"Post_condition: %a\n" pp_predicate_named cond
+	Self.result "Post_condition: %a\n" pp_predicate_named cond
 
 (* Prints a statement *)
 let print_statement stmt =
-	Format.printf"Statement: %a\n" pp_stmt stmt
+	Self.result "Statement: %a\n" pp_stmt stmt
 
 (* Prints a term *)
 let print_term term =
-	Format.printf"Term: %a\n" pp_term term
+	Self.result "Term: %a\n" pp_term term
 
 (* Prints a list of statements *)
 let print_statements list_statements = 
 	List.iter
 		(
-		 fun s -> Format.printf"%a\n" pp_stmt s
+		 fun s -> Self.result "%a\n" pp_stmt s
 		) list_statements
 
 (* Prints a List of tuples of a list of statements and a condition *)
@@ -399,7 +317,8 @@ let print_vcgen_result l =
 	List.iter
 	(
 	 fun (x) -> print_statement x.statement;
-	 			    print_condtion x.predicate
+	 			print_condtion x.predicate;
+	 			print_why3_term x.po.proof_obligation
 	) l
 	
 
@@ -589,14 +508,14 @@ let vcgen list_statements predicate =
 
 (* Returns a reversed list of statements found in fundec.sallstmts after the computation of the cfg *)
 let get_list_of_statements fundec = 
-	Format.printf"Getting list of statements.\n";
+	Self.result "Getting list of statements.\n";
 	let list_statements = fundec.sallstmts in
 	List.rev list_statements
 
 
 (* Get condition depeding ond the func_bulidcondtion input *)
 let get_Condtion  funspec func_buildcondition =
-	Format.printf"Getting Condition.\n";
+	Self.result "Getting Condition.\n";
 	let funbehavior = Cil.find_default_behavior funspec in
 	let post_condition =  func_buildcondition (get_opt funbehavior) Normal in
 	post_condition 
@@ -616,7 +535,7 @@ let apply_if_defition def kf =
 
 (* Visits functions *)
 let visitFunctions () =
-	Format.printf"Visting functions.\n";
+	Self.result "Visting functions.\n";
 	Globals.Functions.fold
 	(
       fun kf acc -> (apply_if_defition (Kernel_function.is_definition kf) kf) @ acc
@@ -626,7 +545,7 @@ let visitFunctions () =
   let run () =
 
      Ast.compute (); 
-     if (Ast.is_computed()) then Format.printf"AST computed.\n"; 	
+     if (Ast.is_computed()) then Self.result "AST computed.\n"; 	
 
      let c_file = Ast.get () in
      Cfg.clearFileCFG c_file;
