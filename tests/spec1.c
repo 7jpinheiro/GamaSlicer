36f9132d0a66784c01f03de2b10ece984138a78e
7jpinheiro <7jpinheiro@gmail.com>
2014-11-03 22:45:23 +0000

    Add Specification-based slice tests

---
+++ tests/spec1.c
@@ -0,0 +1,11 @@
+/*@ requires \true;
+  @ ensures x >= 100;
+*/
+
+void f(int x){
+	x = x*x;
+	x = x+100;
+	x = x+50;
+
+}
+	
---
+++ tests/spec2.c
@@ -0,0 +1,17 @@
+/*@ requires y > 10;
+  @ ensures x >= 0;
+*/
+
+void f(int x,int y){
+	
+	if(y>0){
+	x = x+100;
+	x = x+50;
+	x = x-100;
+	}else{
+	x = x-150;
+    x = x-100;
+	x = x+100;
+	}
+}
+	
